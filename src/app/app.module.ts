e927d6d8852d3b98720204a7ac1c946c6970d855
CarlaKremer <carlakremerf@gmail.com>
2023-12-12 11:35:47 -0300

    added hotjar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { NgxHotjarModule } from 'ngx-hotjar';
+import { environment } from 'src/environments/environment';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgxHotjarModule.forRoot(environment.hj)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  name: 'dev',
+  label: 'ENV_DEV',
+  production: false,
+  hj: "3791279"
+}
